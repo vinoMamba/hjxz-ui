6249f4e9b8a55a004e6ff47205955997f808f876
vino <vino0908@outlook.com>
2023-02-11 18:04:42 +0800

    feat: button 组件添加 disabled

--- packages/preview/src/App.vue
+++ packages/preview/src/App.vue
@@ -1,5 +1,8 @@
 <script setup lang="ts">
 import { DButton } from 'hjxz-design'
+const handleClick = () => {
+  console.log('click')
+}
 </script>
 
 <template>
@@ -8,6 +11,11 @@ import { DButton } from 'hjxz-design'
   <DButton>Default Button</DButton>
   <DButton type="danger">Danger Button</DButton>
   <DButton type="link">Link Button</DButton>
+  <br>
+  <DButton @click="handleClick" disabled type="primary">Primary Button</DButton>
+  <DButton disabled>Default Button</DButton>
+  <DButton disabled type="danger">Danger Button</DButton>
+  <DButton disabled type="link">Link Button</DButton>
 </template>
 
 <style scoped>
--- packages/ui/src/Button/Button.tsx
+++ packages/ui/src/Button/Button.tsx
@@ -9,6 +9,10 @@ export const Button = defineComponent({
       type: String as PropType<'primary' | 'danger' | 'link'>,
       default: '',
     },
+    disabled: {
+      type: Boolean,
+      default: false,
+    },
   },
   setup(props, { slots }) {
     const classes = computed(() => {
@@ -18,7 +22,7 @@ export const Button = defineComponent({
       ]
     })
     return () => (
-      <button class={classes.value}>
+      <button class={classes.value} disabled={props.disabled}>
         <span>
           {slots.default?.()}
         </span>
